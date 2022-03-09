789397c9abb440316d1c2dc5ab0385dcb45f3416
abrahamlp <abrahamlp@gmail.com>
2022-03-09 12:31:05 -0600

    Prueba 3

--- instance.tf
+++ instance.tf
@@ -7,9 +7,12 @@ terraform {
     }
 }
 
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+
 provider "aws" {
-    access_key =  AWS_ACCESS_KEY_ID
-    secret_key = AWS_SECRET_ACCESS_KEY
+    access_key =  var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
     region = "us-east-2"
 }
 
